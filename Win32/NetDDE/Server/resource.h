da4b7425cb824c675c492263f86e72f8545a5f68
Administrator <administrator@chrisoldwood.com>
2003-01-10 22:09:11 +0000

    Added View | Connections men item.

--- Win32/NetDDE/Server/resource.h
+++ Win32/NetDDE/Server/resource.h
@@ -6,7 +6,10 @@
 #define IDR_APPMENU                     3
 #define IDR_APPACCEL                    4
 #define IDR_APPTOOLBAR                  5
+#define ID_FILE_POPUP                   100
 #define ID_FILE_EXIT                    109
+#define ID_VIEW_POPUP                   200
+#define ID_VIEW_CONNECTIONS             201
 #define ID_HELP_POPUP                   900
 #define ID_HELP_CONTENTS                901
 #define ID_HELP_ABOUT                   902
@@ -17,6 +20,7 @@
 #define IDC_TRACE                       1086
 #define IDD_MAIN                        5000
 #define IDD_ABOUT                       5001
+#define IDD_CONNECTIONS                 5002
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -24,9 +28,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        131
-#define _APS_NEXT_COMMAND_VALUE         163
-#define _APS_NEXT_CONTROL_VALUE         1087
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         164
+#define _APS_NEXT_CONTROL_VALUE         1088
 #define _APS_NEXT_SYMED_VALUE           104
 #endif
 #endif
