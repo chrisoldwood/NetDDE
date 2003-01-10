d4cc6ac7707ab3651d660b991c5f367401eae544
Administrator <administrator@chrisoldwood.com>
2003-01-10 13:26:04 +0000

    Added members and methods to store and manipulate the list of conversations and links assocaited with the client connection.

--- Server/NetDDESvrPipe.cpp
+++ Server/NetDDESvrPipe.cpp
@@ -42,4 +42,191 @@ CNetDDESvrPipe::CNetDDESvrPipe()
 CNetDDESvrPipe::~CNetDDESvrPipe()
 {
 	Close();
+
+	// Cleanup.
+	m_aoLinks.RemoveAll();
+	m_aoConvs.RemoveAll();
+}
+
+/******************************************************************************
+** Method:		AddConversation()
+**
+** Description:	Adds the conversation to the list used by this connection.
+**
+** Parameters:	pConv	The conversation.
+**
+** Returns:		Nothing.
+**
+*******************************************************************************
+*/
+
+void CNetDDESvrPipe::AddConversation(CDDECltConv* pConv)
+{
+	ASSERT(pConv != NULL);
+
+	m_aoConvs.Add(pConv);
+}
+
+/******************************************************************************
+** Method:		RemoveConversation()
+**
+** Description:	Removes the conversation from the list used by this connection.
+**				NB: It also removes all links associated with the conversation.
+**
+** Parameters:	pConv	The conversation.
+**
+** Returns:		Nothing.
+**
+*******************************************************************************
+*/
+
+void CNetDDESvrPipe::RemoveConversation(CDDECltConv* pConv)
+{
+	ASSERT(pConv != NULL);
+	ASSERT(UsesConversation(pConv));
+
+	RemoveAllConvLinks(pConv);
+
+	// Remove the conversation.
+	m_aoConvs.Remove(m_aoConvs.Find(pConv));
+}
+
+/******************************************************************************
+** Method:		UsesConversation()
+**
+** Description:	Queries if this connection uses the conversation.
+**
+** Parameters:	pConv	The conversation.
+**
+** Returns:		true or false.
+**
+*******************************************************************************
+*/
+
+bool CNetDDESvrPipe::UsesConversation(CDDECltConv* pConv)
+{
+	ASSERT(pConv != NULL);
+
+	return (m_aoConvs.Find(pConv) != -1);
+}
+
+/******************************************************************************
+** Method:		GetConversations()
+**
+** Description:	Gets the list of conversations used by the connection.
+**
+** Parameters:	aoConvs		The array to return the list.
+**
+** Returns:		Nothing.
+**
+*******************************************************************************
+*/
+
+void CNetDDESvrPipe::GetConversations(CConvs& aoConvs)
+{
+	for (int i = 0; i < m_aoConvs.Size(); ++i)
+		aoConvs.Add(m_aoConvs[i]);
+}
+
+/******************************************************************************
+** Method:		AddLink()
+**
+** Description:	Adds the link to the list used by this connection.
+**
+** Parameters:	pLink	The link.
+**
+** Returns:		Nothing.
+**
+*******************************************************************************
+*/
+
+void CNetDDESvrPipe::AddLink(CDDELink* pLink)
+{
+	ASSERT(pLink != NULL);
+
+	m_aoLinks.Add(pLink);
+}
+
+/******************************************************************************
+** Method:		RemoveLink()
+**
+** Description:	Removes the link from the list used by this connection.
+**
+** Parameters:	pLink	The link.
+**
+** Returns:		Nothing.
+**
+*******************************************************************************
+*/
+
+void CNetDDESvrPipe::RemoveLink(CDDELink* pLink)
+{
+	ASSERT(pLink != NULL);
+	ASSERT(UsesLink(pLink));
+
+	m_aoLinks.Remove(m_aoLinks.Find(pLink));
+}
+
+/******************************************************************************
+** Method:		RemoveAllConvLinks()
+**
+** Description:	Removes all links from the list used by the conversation.
+**
+** Parameters:	pConv	The conversation.
+**
+** Returns:		Nothing.
+**
+*******************************************************************************
+*/
+
+void CNetDDESvrPipe::RemoveAllConvLinks(CDDECltConv* pConv)
+{
+	ASSERT(pConv != NULL);
+	ASSERT(UsesConversation(pConv));
+
+	// Remove all links associated with conversation.
+	for (int i = m_aoLinks.Size()-1; i >= 0; --i)
+	{
+		CDDELink* pLink = m_aoLinks[i];
+
+		if (pLink->Conversation() == pConv)
+			m_aoLinks.Remove(i);
+	}
+}
+
+/******************************************************************************
+** Method:		UsesLink()
+**
+** Description:	Queries if this connection uses the link.
+**
+** Parameters:	pLink	The link.
+**
+** Returns:		true or false.
+**
+*******************************************************************************
+*/
+
+bool CNetDDESvrPipe::UsesLink(CDDELink* pLink)
+{
+	ASSERT(pLink != NULL);
+
+	return (m_aoLinks.Find(pLink) != -1);
+}
+
+/******************************************************************************
+** Method:		GetLinks()
+**
+** Description:	Gets the list of links used by the connection.
+**
+** Parameters:	aoLinks		The array to return the list.
+**
+** Returns:		Nothing.
+**
+*******************************************************************************
+*/
+
+void CNetDDESvrPipe::GetLinks(CLinks& aoLinks)
+{
+	for (int i = 0; i < m_aoLinks.Size(); ++i)
+		aoLinks.Add(m_aoLinks[i]);
 }
--- Server/NetDDESvrPipe.hpp
+++ Server/NetDDESvrPipe.hpp
@@ -12,6 +12,10 @@
 #ifndef NETDDESVRPIPE_HPP
 #define NETDDESVRPIPE_HPP
 
+// Template shorthands.
+typedef TPtrArray<CDDECltConv> CConvs;
+typedef TPtrArray<CDDELink> CLinks;
+
 /******************************************************************************
 ** 
 ** The server end of a NetDDE pipe.
@@ -27,11 +31,30 @@ public:
 	//
 	CNetDDESvrPipe();
 	~CNetDDESvrPipe();
-	
+
+	//
+	// Conversation methods.
+	//
+	void AddConversation(CDDECltConv* pConv);
+	void RemoveConversation(CDDECltConv* pConv);
+	bool UsesConversation(CDDECltConv* pConv);
+	void GetConversations(CConvs& aoConvs);
+
+	//
+	// Link methods.
+	//
+	void AddLink(CDDELink* pLink);
+	void RemoveLink(CDDELink* pLink);
+	void RemoveAllConvLinks(CDDECltConv* pConv);
+	bool UsesLink(CDDELink* pLink);
+	void GetLinks(CLinks& aoLinks);
+
 protected:
 	//
 	// Members.
 	//
+	CConvs	m_aoConvs;	// DDE Conversation list.
+	CLinks	m_aoLinks;	// DDE links list.
 };
 
 /******************************************************************************
