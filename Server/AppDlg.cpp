32e3b1319d744221670a0766118db8ae2521ed38
Administrator <administrator@chrisoldwood.com>
2003-01-10 13:23:32 +0000

    Added trace child window and a Trace() method to add a line of trace.

--- Server/AppDlg.cpp
+++ Server/AppDlg.cpp
@@ -25,15 +25,13 @@
 CAppDlg::CAppDlg()
 	: CMainDlg(IDD_MAIN)
 {
-/*
 	DEFINE_CTRL_TABLE
-		CTRL(IDC_GRID,	&m_lvGrid)
+		CTRL(IDC_TRACE,	&m_lbTrace)
 	END_CTRL_TABLE
 
 	DEFINE_GRAVITY_TABLE
-		CTRLGRAV(IDC_GRID, LEFT_EDGE, TOP_EDGE, RIGHT_EDGE, BOTTOM_EDGE)
+		CTRLGRAV(IDC_TRACE, LEFT_EDGE, TOP_EDGE, RIGHT_EDGE, BOTTOM_EDGE)
 	END_GRAVITY_TABLE
-*/
 }
 
 /******************************************************************************
@@ -50,4 +48,29 @@ CAppDlg::CAppDlg()
 
 void CAppDlg::OnInitDialog()
 {
+	m_lbTrace.Font(CFont(ANSI_FIXED_FONT));
+}
+
+/******************************************************************************
+** Method:		Trace()
+**
+** Description:	Add a line of trace to the window.
+**
+** Parameters:	pszMsg		The trace message.
+**
+** Returns:		Nothing.
+**
+*******************************************************************************
+*/
+
+void CAppDlg::Trace(const char* pszMsg)
+{
+	uint i = m_lbTrace.Add(pszMsg);
+
+	m_lbTrace.CurSel(i);
+
+	if (i >= App.m_nMaxTrace)
+		m_lbTrace.Delete(0);
+
+	m_lbTrace.Update();
 }
