4859f9db093fbe5a70c23e373466e1f2c3c0cfd4
Administrator <administrator@chrisoldwood.com>
2004-09-18 17:07:10 +0000

    Vss2Git

---
+++ Server/OptionsDlg.hpp
@@ -0,0 +1,57 @@
+/******************************************************************************
+** (C) Chris Oldwood
+**
+** MODULE:		OPTIONSDLG.HPP
+** COMPONENT:	The Application
+** DESCRIPTION:	The COptionsDlg class declaration.
+**
+*******************************************************************************
+*/
+
+// Check for previous inclusion
+#ifndef OPTIONSDLG_HPP
+#define OPTIONSDLG_HPP
+
+/******************************************************************************
+** 
+** The dialog used to configure the general settings.
+**
+*******************************************************************************
+*/
+
+class COptionsDlg : public CDialog
+{
+public:
+	//
+	// Constructors/Destructor.
+	//
+	COptionsDlg();
+	
+protected:
+	//
+	// Members.
+	//
+	
+	//
+	// Controls.
+	//
+	CCheckBox	m_ckTrayIcon;
+	CCheckBox	m_ckMinToTray;
+
+	//
+	// Message handlers.
+	//
+	virtual void OnInitDialog();
+	virtual bool OnOk();
+
+	void OnMinToTrayClicked();
+};
+
+/******************************************************************************
+**
+** Implementation of inline functions.
+**
+*******************************************************************************
+*/
+
+#endif // OPTIONSDLG_HPP
---
+++ Shared/NetDDEShared.hpp
@@ -0,0 +1,41 @@
+/******************************************************************************
+** (C) Chris Oldwood
+**
+** MODULE:		NETDDESHARED.HPP
+** COMPONENT:	The Application.
+** DESCRIPTION:	Wrapper to include all the shared application headers.
+**
+*******************************************************************************
+*/
+
+// Check for previous inclusion
+#ifndef NETDDESHARED_HPP
+#define NETDDESHARED_HPP
+
+/******************************************************************************
+**
+** System headers.
+**
+*******************************************************************************
+*/
+
+#include "wcl.hpp"		// Windows C++ library and core headers.
+#include "ncl.hpp"		// Network & Comms library headers.
+
+/******************************************************************************
+**
+** Application specific headers.
+**
+*******************************************************************************
+*/
+
+#include "Resource.h"
+
+// Data classes.
+#include "LinkValue.hpp"
+#include "LinkCache.hpp"
+#include "NetDDEDefs.hpp"
+#include "NetDDEPacket.hpp"
+#include "NetDDEPipe.hpp"
+
+#endif //NETDDESHARED_HPP
