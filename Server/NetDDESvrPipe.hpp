33d31f972e7fef4f7dc4ed44fe8b78b961cfb3da
Administrator <administrator@chrisoldwood.com>
2004-10-01 21:41:14 +0000

    Made members public and removed the largely redudant conversation and link methods. Changed to store a separate CNetDEEConv object for each peer client conversation to ensure they are handled properly. Replaced UsesLink() method with IsLinkUsed().

--- Server/NetDDESvrPipe.hpp
+++ Server/NetDDESvrPipe.hpp
@@ -13,8 +13,7 @@
 #define NETDDESVRPIPE_HPP
 
 // Template shorthands.
-typedef TPtrArray<CDDECltConv> CConvs;
-typedef TPtrArray<CDDELink> CLinks;
+typedef TPtrArray<CNetDDEConv> CNetConvs;
 
 /******************************************************************************
 ** 
@@ -33,45 +32,20 @@ public:
 	~CNetDDESvrPipe();
 
 	//
-	// Properties.
+	// Methods.
 	//
-	const CString& Service() const;
-	void Service(const CString& strService);
+	CNetDDEConv* FindNetConv(CDDECltConv* pConv, uint32 nConvID) const;
 
-	const CString& Computer() const;
-	void Computer(const CString& strComputer);
+	bool IsLinkUsed(CDDELink* pLink) const;
 
-	const CString& User() const;
-	void User(const CString& strUser);
-
-	//
-	// Conversation methods.
-	//
-	void AddConversation(CDDECltConv* pConv);
-	void RemoveConversation(CDDECltConv* pConv);
-	bool UsesConversation(CDDECltConv* pConv);
-	void GetConversations(CConvs& aoConvs);
-	int  NumConversations() const;
-
-	//
-	// Link methods.
-	//
-	void AddLink(CDDELink* pLink);
-	void RemoveLink(CDDELink* pLink);
-	void RemoveAllConvLinks(CDDECltConv* pConv);
-	bool UsesLink(CDDELink* pLink);
-	void GetLinks(CLinks& aoLinks);
-	int  NumLinks() const;
-
-protected:
+public:
 	//
 	// Members.
 	//
-	CString	m_strService;	// DDE Service name.
-	CString	m_strComputer;	// Computer name.
-	CString m_strUser;		// User name.
-	CConvs	m_aoConvs;		// DDE Conversation list.
-	CLinks	m_aoLinks;		// DDE links list.
+	CString		m_strService;	// DDE Service name.
+	CString		m_strComputer;	// Computer name.
+	CString		m_strUser;		// User name.
+	CNetConvs	m_aoNetConvs;	// NetDDE Conversation list.
 };
 
 /******************************************************************************
@@ -80,45 +54,5 @@ protected:
 **
 *******************************************************************************
 */
-
-inline const CString& CNetDDESvrPipe::Service() const
-{
-	return m_strService;
-}
-
-inline void CNetDDESvrPipe::Service(const CString& strService)
-{
-	m_strService = strService;
-}
-
-inline const CString& CNetDDESvrPipe::Computer() const
-{
-	return m_strComputer;
-}
-
-inline void CNetDDESvrPipe::Computer(const CString& strComputer)
-{
-	m_strComputer = strComputer;
-}
-
-inline const CString& CNetDDESvrPipe::User() const
-{
-	return m_strUser;
-}
-
-inline void CNetDDESvrPipe::User(const CString& strUser)
-{
-	m_strUser = strUser;
-}
-
-inline int CNetDDESvrPipe::NumConversations() const
-{
-	return m_aoConvs.Size();
-}
-
-inline int CNetDDESvrPipe::NumLinks() const
-{
-	return m_aoLinks.Size();
-}
 
 #endif // NETDDESVRPIPE_HPP
