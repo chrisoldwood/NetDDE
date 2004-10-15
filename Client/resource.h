0913b7a88fbaa026288414061cef1a72f6d558a4
Administrator <administrator@chrisoldwood.com>
2004-10-15 23:08:09 +0000

    Added system tray context menu. Changed ListViews to have "always shwo selection" on. Added new settings to the "Options" dialog.

--- Client/resource.h
+++ Client/resource.h
@@ -6,6 +6,7 @@
 #define IDR_APPMENU                     3
 #define IDR_APPACCEL                    4
 #define IDR_APPTOOLBAR                  5
+#define IDR_TRAYMENU                    6
 #define IDI_NET_IDLE                    10
 #define IDI_NET_LOST                    11
 #define IDI_NET_SEND                    12
@@ -23,6 +24,8 @@
 #define ID_HELP_POPUP                   900
 #define ID_HELP_CONTENTS                901
 #define ID_HELP_ABOUT                   902
+#define ID_TRAY_RESTORE                 1001
+#define ID_TRAY_EXIT                    1002
 #define IDC_GRID                        1060
 #define IDC_WEBSITE                     1083
 #define IDC_EMAIL                       1084
@@ -32,6 +35,7 @@
 #define IDC_ADVISES                     1087
 #define IDC_UPDATES                     1088
 #define IDC_REQUESTS                    1089
+#define IDC_NET_TIMEOUT                 1089
 #define IDC_WINDOW                      1094
 #define IDC_FILE                        1095
 #define IDC_CONNECTIONS                 1096
@@ -52,7 +56,6 @@
 #define IDC_BAD_ADVISE_VAL              1110
 #define IDC_LINKS                       1110
 #define IDC_REQ_VALUE                   1111
-#define IDC_BUTTON1                     1111
 #define IDC_DISCONNECT                  1111
 #define IDC_LOCAL_NAME                  1112
 #define IDD_SERVICE                     2037
@@ -72,8 +75,8 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        2039
-#define _APS_NEXT_COMMAND_VALUE         3069
+#define _APS_NEXT_RESOURCE_VALUE        2040
+#define _APS_NEXT_COMMAND_VALUE         3073
 #define _APS_NEXT_CONTROL_VALUE         1112
 #define _APS_NEXT_SYMED_VALUE           104
 #endif
