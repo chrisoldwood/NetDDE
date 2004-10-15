ac06bde4273b13b3f9013784df24a6e60bdc7dba
Administrator <administrator@chrisoldwood.com>
2004-10-15 23:08:09 +0000

    Added system tray context menu. Changed ListViews to have "always shwo selection" on. Added new settings to the "Options" dialog.

--- Win32/NetDDE/Server/resource.h
+++ Win32/NetDDE/Server/resource.h
@@ -6,6 +6,7 @@
 #define IDR_APPMENU                     3
 #define IDR_APPACCEL                    4
 #define IDR_APPTOOLBAR                  5
+#define IDR_TRAYMENU                    6
 #define IDI_NET_IDLE                    10
 #define IDI_NET_LOST                    11
 #define IDI_NET_SEND                    12
@@ -22,6 +23,8 @@
 #define ID_HELP_POPUP                   900
 #define ID_HELP_CONTENTS                901
 #define ID_HELP_ABOUT                   902
+#define ID_TRAY_RESTORE                 1001
+#define ID_TRAY_EXIT                    1002
 #define IDC_GRID                        1060
 #define IDC_WEBSITE                     1083
 #define IDC_EMAIL                       1084
@@ -30,7 +33,11 @@
 #define IDC_CONVERSATIONS               1086
 #define IDC_ADVISES                     1087
 #define IDC_UPDATES                     1088
+#define IDC_DISCARD_DUPS                1088
 #define IDC_REQUESTS                    1089
+#define IDC_NET_TIMEOUT                 1089
+#define IDC_NET_TIMEOUT2                1090
+#define IDC_DDE_TIMEOUT                 1090
 #define IDC_WINDOW                      1094
 #define IDC_FILE                        1095
 #define IDC_CONNECTIONS                 1096
@@ -39,6 +46,7 @@
 #define IDC_MIN_TO_TRAY                 1101
 #define IDC_TRAY_ICON                   1102
 #define IDC_LINKS                       1110
+#define IDC_DISCONNECT                  1111
 #define IDD_LINKS                       2038
 #define ID_VIEW_CLEAR_TRACE             3068
 #define IDD_MAIN                        5000
@@ -56,7 +64,7 @@
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        132
 #define _APS_NEXT_COMMAND_VALUE         165
-#define _APS_NEXT_CONTROL_VALUE         1088
+#define _APS_NEXT_CONTROL_VALUE         1090
 #define _APS_NEXT_SYMED_VALUE           104
 #endif
 #endif
