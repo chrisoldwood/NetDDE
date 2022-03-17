2941af10e2d6049e846b00a1072782a6c5d5ed0b
Chris Oldwood <gort@cix.co.uk>
2022-03-17 08:27:01 +0000

    Added a project for unit tests.

---
+++ Test/Test.cpp
@@ -0,0 +1,13 @@
+////////////////////////////////////////////////////////////////////////////////
+//! \file   Test.cpp
+//! \brief  The test harness entry point.
+//! \author Chris Oldwood
+
+#include "Common.hpp"
+#include <tchar.h>
+#include <Core/UnitTest.hpp>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	TEST_SUITE_MAIN(argc, argv);
+}
